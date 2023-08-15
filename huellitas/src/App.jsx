1c0b22486068fcf9a4e2892f3ab0872e81e46fcc
Pablo <pablomd_98@hotmail.com>
2023-08-14 21:30:25 -0300

    instalo rutas

--- huellitas/src/App.jsx
+++ huellitas/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import PaginaPrincipal from './pages/paginaPrincipal/paginaPrincipal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 
 const App = () => {
   return (
     <>
-    <PaginaPrincipal />
-    
+
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<PaginaPrincipal />} />
+          <Route path="/" element={<Planes />} />
+        </Routes>
+      </Router>
+
+      <PaginaPrincipal />
+
     </>
   )
 }
