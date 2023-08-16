1f376cf3d64605cab08dae710419ad333d5a06b1
DanielSpiner <danielspr.33@gmail.com>
2023-08-16 14:05:33 -0300

    se manda push de about, falta solucionar detalles de la pagina y de react

---
+++ huellitas/src/Pages/About.jsx
@@ -0,0 +1,158 @@
+import "./about.css"
+
+
+
+function about() {
+    return (
+      <>
+       <body class="colorfondo">
+    <div>
+      <h2 class="text-center my-5">Quienes somos?</h2>
+    </div>
+    {/* <!-- contenedor 1 --> */}
+
+    <div class="container fach text-center p-5">
+      <p>
+        Bienvenido/a a nuestra página web de hardware, el destino perfecto para
+        los entusiastas de la tecnología y los amantes de la informática. Aquí
+        en nuestra tienda online, encontrarás una amplia selección de
+        componentes de hardware de última generación, diseñados para potenciar y
+        mejorar tu experiencia tecnológica.
+        <br />
+      </p>
+      <p>
+        Nuestro catálogo abarca una gran variedad de productos, desde potentes
+        procesadores y tarjetas gráficas de alto rendimiento, hasta placas base
+        confiables, memorias RAM de velocidad sobresaliente y unidades de
+        almacenamiento ultrarrápidas. También ofrecemos periféricos de calidad,
+        como teclados, ratones, monitores y auriculares, que te brindarán una
+        interacción fluida y envolvente con tu equipo.
+      </p>
+      <p>
+        Trabajamos con los principales fabricantes de hardware, lo que nos
+        permite ofrecer productos de calidad excepcional y rendimiento
+        garantizado. Nuestro equipo de expertos está siempre dispuesto a
+        brindarte asesoramiento personalizado y orientación para ayudarte a
+        encontrar la solución perfecta que se ajuste a tus necesidades y
+        presupuesto.
+      </p>
+    </div>
+
+    <div class="container my-5">
+      <hr />
+    </div>
+
+    {/* <!-- contenedor 1 --> */}
+
+    <div class="container d-flex flex-wrap justify-content-center">
+      <div class="d-flex mx-5 my-4">
+        <div class="card fach cambiocolor" style="width: 20rem; height: 23rem">
+          <div class="d-flex justify-content-center mt-4">
+            <img
+              src=""
+              class="card-img-top fotoCircle"
+              alt="..."
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-text text-center mt-3">Daniel Spiner</h4>
+          </div>
+          <div class="d-flex justify-content-center mb-4">
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook text-dark mx-2"></i></a>
+            <a href="https://twitter.com/"><i class="bi bi-twitter text-dark mx-2"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram text-dark mx-2"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex mx-5 my-4">
+        <div class="card fach cambiocolor" style="width: 20rem; height: 23rem">
+          <div class="d-flex justify-content-center mt-4">
+            <img src="" class="card-img-top fotoCircle" alt="..." />
+          </div>
+          <div class="card-body">
+            <h4 class="card-text text-center mt-3">Daniel Spiner</h4>
+          </div>
+          <div class="d-flex justify-content-center mb-4">
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook text-dark mx-2"></i></a>
+            <a href="https://twitter.com/"><i class="bi bi-twitter text-dark mx-2"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram text-dark mx-2"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex mx-5 my-4">
+        <div class="card fach cambiocolor" style="width: 20rem; height: 23rem">
+          <div class="d-flex justify-content-center mt-4">
+            <img
+              src=""
+              class="card-img-top fotoCircle"
+              alt="..."
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-text text-center mt-3">Daniel Spiner</h4>
+          </div>
+          <div class="d-flex justify-content-center mb-4">
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook text-dark mx-2"></i></a>
+            <a href="https://twitter.com/"><i class="bi bi-twitter text-dark mx-2"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram text-dark mx-2"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex mx-5 my-4">
+        <div class="card fach cambiocolor" style="width: 20rem; height: 23rem">
+          <div class="d-flex justify-content-center mt-4">
+            <img
+              src=""
+              class="card-img-top fotoCircle"
+              alt="..."
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-text text-center mt-3">Daniel Spiner</h4>
+          </div>
+          <div class="d-flex justify-content-center mb-4">
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook text-dark mx-2"></i></a>
+            <a href="https://twitter.com/"><i class="bi bi-twitter text-dark mx-2"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram text-dark mx-2"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex mx-5 my-4">
+        <div class="card fach cambiocolor" style="width: 20rem; height: 23rem">
+          <div class="d-flex justify-content-center mt-4">
+            <img
+              src=""
+              class="card-img-top fotoCircle"
+              alt="..."
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-text text-center mt-3">Daniel Spiner</h4>
+          </div>
+          <div class="d-flex justify-content-center mb-4">
+            <a href="https://www.facebook.com/"><i class="bi bi-facebook text-dark mx-2"></i></a>
+            <a href="https://twitter.com/"><i class="bi bi-twitter text-dark mx-2"></i></a>
+            <a href="https://www.instagram.com/"><i class="bi bi-instagram text-dark mx-2"></i></a>
+          </div>
+        </div>
+      </div>
+
+               
+    </div>
+
+
+  </body>
+      
+
+  
+      </>
+    );
+  }
+  
+  export default about;
+
+
