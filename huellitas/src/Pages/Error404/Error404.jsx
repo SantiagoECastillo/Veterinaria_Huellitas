2d17fecba805d3d992f31b570e534c3190e630aa
DanielSpiner <danielspr.33@gmail.com>
2023-08-19 07:32:34 -0300

    Se cambia detalles de la pagina error, se cambia color de fondo, falta cambiar el tipo de letras

--- huellitas/src/Pages/Error404/Error404.jsx
+++ huellitas/src/Pages/Error404/Error404.jsx
@@ -3,34 +3,37 @@ import "./error404.css";
 function Error404() {
   return (
     <>
-      <body className=" vh-100">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-3">
-              
-            </div>
+      <div className="container vh-100">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-3"></div>
 
-            <div className="col-12 col-sm-12 col-md-12 col-lg-6">
-              <div className="main-img text-center mt-5 tamanoCont">
-                
-                <img
-                  src="/src/images/error404.jpg"
-                  alt="perro"
-                  className="img-fluid"
-                />
-                <h1>Pagina no encontrada</h1>
-                <p className="main-description pt-2">
-                  Lo lamentamos. Parece que te has perdido
-                </p>
-                <buttom className="btn btn-sz-primary">Inicio</buttom>
-                </div>
-                
-            </div>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-6">
+            <h1 className="text-center my-5">
+              <u>Pagina no encontrada</u>
+            </h1>
+            <div className="main-img text-center pt-5 colorFondo">
+              <img
+                src="/src/images/perro.jpg"
+                alt="perro"
+                className="img-fluid"
+              />
 
-            <div className="col-12 col-sm-12 col-md-12 col-lg-3"></div>
+              <p className="main-description pt-5">
+                el enlace que seguiste probablemente está roto o la página ha
+                sido eliminada
+              </p>
+              <p className=" main-description">
+                Haz clic en el botón para volver a la página principal.
+              </p>
+              <buttom className="btn btn-sz-primary mt-5">
+                <b>Volver al inicio</b>
+              </buttom>
+            </div>
           </div>
+
+          <div className="col-12 col-sm-12 col-md-12 col-lg-3"></div>
         </div>
-      </body>
+      </div>
     </>
   );
 }
