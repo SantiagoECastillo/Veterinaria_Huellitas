7b7b8ddeb2faf2b42d859df252e547f03a905de9
Pablo <pablomd_98@hotmail.com>
2023-08-19 21:13:13 -0300

    se crea detalle de planes con email de auto respuesta funcionando

--- huellitas/src/pages/detallePlanes/DetallePlanes.jsx
+++ huellitas/src/pages/detallePlanes/DetallePlanes.jsx
@@ -1,43 +1,104 @@
-import "./DetallePlanes.css"
+import { Button, Modal } from "react-bootstrap";
+import imagenConclusion from "../../images/mujerconperro2.jpg";
+import "./DetallePlanes.css";
+import { useState } from "react";
+import ModalFormPlanes from "../../components/planes/ModalFormPlanes";
 
 const Planes = () => {
-    return (
-        <>
-            <div className="container">
+  const [show, setShow] = useState(false);
 
-                <h1>Nuestros Planes</h1>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-                <div>
-                    <div className="plan-container">
+  const modalDatos = () => {
+    handleShow();
+  };
 
-                        <p className="plan-name">Primeros Pasos:</p>
-                        <p className="plan-description"> Nuestro Plan "Primeros Pasos" está diseñado para proporcionar una atención esencial y accesible para tus mascotas. Incluye exámenes de salud anuales, vacunas importantes para prevenir enfermedades comunes, y desparasitación regular para mantener a tu compañero peludo protegido contra parásitos internos y externos. Es ideal para propietarios que buscan cuidados preventivos fundamentales a un precio asequible.</p>
-
-                    </div>
-                    <div className="plan-container">
-
-                        <p className="plan-name">Madurando:</p>
-                        <p className="plan-description"> El Plan "Madurando" lleva el cuidado de tu mascota al siguiente nivel al ofrecer una gama más amplia de servicios. Además de los exámenes anuales y las vacunas necesarias, este plan abarca análisis de sangre y orina para detectar problemas de salud temprano, lo que permite un tratamiento más efectivo. También incluye consultas ilimitadas, lo que brinda tranquilidad a los dueños de mascotas al saber que pueden acceder a la atención veterinaria cuando la necesiten.</p>
-
-                    </div>
-                    <div className="plan-container">
-
-                        <p className="plan-name">Adultos:</p>
-                        <p className="plan-description"> Nuestro Plan "Adultos" es la opción más completa y avanzada para el bienestar de tu mascota. Ofrecemos exámenes médicos exhaustivos que abarcan desde radiografías hasta ecografías, permitiendo una evaluación profunda de la salud interna. Este plan cubre cirugías programadas, así como procedimientos especializados para abordar problemas de salud específicos. También incluye acceso a especialistas veterinarios y terapias avanzadas, garantizando que tu mascota reciba la atención más completa y avanzada disponible.</p>
-
-                    </div>
-
-                    <div className="conclusion">
-
-                        <p>Cada uno de nuestros planes está diseñado para adaptarse a las diferentes necesidades de cuidado de tu mascota, brindándote opciones flexibles y confiables para mantener a tu compañero peludo saludable y feliz.</p>
-
-                    </div>
-
-                </div>
-
+  return (
+    <>
+      <main className="containerDetallePlanes container">
+          <h1>Nuestros Planes</h1>
+        <div className="row gx5">
+          <div className="contenedorConclusion col">
+            <div className="conclusion">
+              <div className="textoConclusion">
+                <p>
+                  Cada uno de nuestros planes está diseñado para adaptarse a las
+                  diferentes necesidades de cuidado de tu mascota, brindándote
+                  opciones flexibles y confiables para mantener a tu compañero
+                  peludo saludable y feliz.
+                </p>
+              </div>
+              <div className="contenedorImgConcl">
+                <img className="imagenConclusion" src={imagenConclusion} />
+                <Button className="botonModal" onClick={modalDatos}>
+                  MAS INFO
+                </Button>
+              </div>
             </div>
-            <h1>Prueba</h1>
-        </>
-    )
-}
-export default Planes
+          </div>
+          <div className="contenedorPlanes col">
+            <div className="plan-container">
+              <div className="plan-name">PRIMEROS PASOS</div>
+              <div className="plan-description">
+                Nuestro Plan "Primeros Pasos" está diseñado para proporcionar
+                una atención esencial y accesible para tus mascotas. Incluye
+                exámenes de salud anuales, vacunas importantes para prevenir
+                enfermedades comunes, y desparasitación regular para mantener a
+                tu compañero peludo protegido contra parásitos internos y
+                externos. Es ideal para propietarios que buscan cuidados
+                preventivos fundamentales a un precio asequible.
+              </div>
+            </div>
+            <div className="plan-container">
+              <div className="plan-name">MADURANDO</div>
+              <div className="plan-description">
+                El Plan "Madurando" lleva el cuidado de tu mascota al siguiente
+                nivel al ofrecer una gama más amplia de servicios. Además de los
+                exámenes anuales y las vacunas necesarias, este plan abarca
+                análisis de sangre y orina para detectar problemas de salud
+                temprano, lo que permite un tratamiento más efectivo. También
+                incluye consultas ilimitadas, lo que brinda tranquilidad a los
+                dueños de mascotas al saber que pueden acceder a la atención
+                veterinaria cuando la necesiten.
+              </div>
+            </div>
+            <div className="plan-container">
+              <div className="plan-name">ADULTOS</div>
+              <div className="plan-description">
+                Nuestro Plan "Adultos" es la opción más completa y avanzada para
+                el bienestar de tu mascota. Ofrecemos exámenes médicos
+                exhaustivos que abarcan desde radiografías hasta ecografías,
+                permitiendo una evaluación profunda de la salud interna. Este
+                plan cubre cirugías programadas, así como procedimientos
+                especializados para abordar problemas de salud específicos.
+                También incluye acceso a especialistas veterinarios y terapias
+                avanzadas, garantizando que tu mascota reciba la atención más
+                completa y avanzada disponible.
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Modal
+        className="modalPlanes"
+        centered
+        size="lg"
+        show={show}
+        onHide={handleClose}
+      >
+        <div className="contenedorModalPlanes">
+        <Modal.Header className="headerModalPlanes" closeButton>
+          <Modal.Title className="tituloModalPlanes">
+            MAS INFO DE PLANES
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bodyModalPlanes">
+          <ModalFormPlanes handleClose={handleClose} />
+        </Modal.Body>
+        </div>
+      </Modal>
+    </>
+  );
+};
+export default Planes;
