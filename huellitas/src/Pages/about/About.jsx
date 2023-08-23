4fc9f892a1bdd733315763c0cf6f8da7cde38e56
Pablo <pablomd_98@hotmail.com>
2023-08-23 02:08:41 -0300

    se corrigio colores y descripcion del equipo

--- huellitas/src/Pages/about/About.jsx
+++ huellitas/src/Pages/about/About.jsx
@@ -4,47 +4,23 @@ function About() {
   return (
     <>
       <div>
-      <h2 className="text-center my-4"><b><u>¿Quiénes somos?</u></b></h2>
+        <h1 className="text-center my-4"><b>¿Quiénes somos?</b></h1>
       </div>
 
-      {/* Contenedor 1 */}
-      <div className="container fach text-center p-5">
+
+      <div className="containerAbout fach text-center p-5">
         <p>
-          En Huellitas, comprendemos que tus mascotas son parte de tu familia.
-          Nuestro compromiso es proporcionarles el mejor cuidado veterinario en
-          un ambiente cálido y acogedor. Con años de experiencia en la atención
-          de animales de compañía, nuestro equipo de veterinarios altamente
-          capacitados se dedica a mantener a tus amigos peludos felices y
-          saludables en cada etapa de sus vidas.
-        </p>
-        <p>
-          Ofrecemos una amplia gama de servicios veterinarios diseñados para
-          satisfacer todas las necesidades de tus mascotas. Desde chequeos de
-          rutina, vacunación y desparasitación, hasta cirugías especializadas y
-          cuidados intensivos, estamos preparados para brindar la atención
-          médica que tus animales merecen. Ya sea que tengas un cachorro
-          juguetón, un gato curioso o una mascota mayor que necesita cuidados
-          especiales, estamos aquí para ayudar.
-        </p>
-        <p>
-          Nuestro equipo está formado por apasionados amantes de los animales
-          que se esfuerzan por establecer conexiones duraderas con nuestros
-          pacientes y sus dueños. Entendemos que cada mascota es única, y
-          trabajamos en colaboración contigo para desarrollar planes de atención
-          personalizados que se adapten a las necesidades individuales de tu
-          compañero peludo.
+          Somos un equipo de desarrolladores Full Stack apasionados por los animales y la tecnología.
+          Juntos, creamos 'Huellitas', una página de veterinaria que une nuestra experiencia en desarrollo con nuestro amor por los amigos peludos.
+          Con una interfaz amigable, permitimos a los dueños de mascotas acceder fácilmente a servicios veterinarios y programar citas.
+          Nuestra misión es mejorar la vida de las mascotas y sus dueños a través de la innovación tecnológica y el cuidado dedicado.
         </p>
       </div>
 
-      <div className="container my-5">
-        <hr />
-      </div>
-      {/* Fin de Contenedor 1 */}
 
-      <h2 className="text-center"><b><u>Nuestro equipo</u></b></h2>
+      <h2><b>Nuestro equipo:</b></h2>
 
-      {/* Contenedor 2 */}
-      <div className="container d-flex flex-wrap justify-content-center">
+      <div className="containerCardsAbout d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-4 my-4">
           <div
             className="card fach cambiocolor"
@@ -55,10 +31,7 @@ function About() {
               alt="esteban"
               className="fotoCircle mx-4 mt-4"
             />
-            <h3 className="text-center mt-4"><b>Pablo Decima</b></h3>
-            <p className="text-center">
-            Médico veterinario
-           </p>
+            <h3 className="text-center mt-4"><b>Pablo Décima</b></h3>
           </div>
         </div>
 
@@ -73,9 +46,6 @@ function About() {
               className="fotoCircle mx-4 mt-4"
             />
             <h3 className="text-center mt-4"><b>Daniel Spiner</b></h3>
-            <p className="text-center">
-            Médico veterinario 
-           </p>
           </div>
         </div>
 
@@ -90,29 +60,19 @@ function About() {
               className="fotoCircle mx-4 mt-4"
             />
             <h3 className="text-center mt-4"><b>Esteban Pedraza</b></h3>
-            <p className="text-center">
-            Médico veterinario
-           </p>
           </div>
         </div>
-      </div>
-           
-
-      <div className="container d-flex flex-wrap justify-content-center">
         <div className="d-flex mx-5 my-4">
           <div
             className="card fach cambiocolor"
             style={{ width: "20rem", height: "23rem" }}
           >
-          <img
+            <img
               src="/src/images/integrantes/Estebanp.jpg"
               alt="esteban"
               className="fotoCircle mx-4 mt-4"
             />
-            <h3 className="text-center mt-4"><b>Esteban Pedraza</b></h3>
-            <p className="text-center">
-            Auxiliar Veterinario
-           </p>
+            <h3 className="text-center mt-4"><b>Santiago Castillo</b></h3>
           </div>
         </div>
 
@@ -127,14 +87,10 @@ function About() {
               className="fotoCircle mx-4 mt-4"
             />
             <h3 className="text-center mt-4"><b> Alberto Aguirre</b></h3>
-            <p className="text-center">
-            Auxiliar Veterinario
-           </p>
           </div>
         </div>
       </div>
 
-      {/* Fin de Contenedor 2 */}
     </>
   );
 }
