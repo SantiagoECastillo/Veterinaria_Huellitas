d0497ffc4ddee0def22232dcff524df791d7d34a
Pablo <pablomd_98@hotmail.com>
2023-08-23 01:38:36 -0300

    estilos a la pagina error

--- huellitas/src/Pages/Error404/Error404.jsx
+++ huellitas/src/Pages/Error404/Error404.jsx
@@ -8,8 +8,8 @@ function Error404() {
           <div className="col-12 col-sm-12 col-md-12 col-lg-3"></div>
 
           <div className="col-12 col-sm-12 col-md-12 col-lg-6">
-            <h1 className="text-center my-5">
-              <u>Pagina no encontrada</u>
+            <h1 className="text-center my-5 neon-text">
+              Pagina no encontrada
             </h1>
             <div className="main-img text-center pt-5 colorFondo">
               <img
@@ -17,14 +17,11 @@ function Error404() {
                 alt="perro"
                 className="img-fluid"
               />
-
               <p className="main-description pt-5">
-                el enlace que seguiste probablemente está roto o la página ha
+                El enlace que seguiste probablemente no está funcionando o la página ha
                 sido eliminada
               </p>
-              <p className=" main-description">
-                Haz clic en el botón para volver a la página principal.
-              </p>
+
               <buttom className="btn btn-sz-primary mt-5">
                 <b>Volver al inicio</b>
               </buttom>
