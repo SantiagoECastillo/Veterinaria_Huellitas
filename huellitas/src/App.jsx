5bac96cdbbc6e55c01d1cab7fcec9bed4c6039ef
Pablo <pablomd_98@hotmail.com>
2023-08-23 23:50:48 -0300

    se corrige app

--- huellitas/src/App.jsx
+++ huellitas/src/App.jsx
@@ -1,12 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useState } from 'react'
 import Error404 from "./Pages/Error404/Error404"
 
 /* import './App.css' */
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = ()=> {
+
 
   return (
     <>
