0bf74bd5659ede66ec2277a750e7ce9ce2587ebb
Pablo <pablomd_98@hotmail.com>
2023-08-24 00:03:44 -0300

    ultimos detalles corregidos

--- huellitas/src/App.jsx
+++ huellitas/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UsuariosContexto from './Context/UsuariosContexto';
 
-function App() {
+const App= () => {
 
   return (
     <>
       <UsuariosContexto>
-        <Header />
+        
       </UsuariosContexto>
     </>
   )
