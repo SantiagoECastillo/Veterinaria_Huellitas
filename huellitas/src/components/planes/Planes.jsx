00d0d0df00944b9c31681a2d2355c4ac70a07477
Pablo <pablomd_98@hotmail.com>
2023-08-24 03:38:59 -0300

    correcion de errores del merge

--- huellitas/src/components/planes/Planes.jsx
+++ huellitas/src/components/planes/Planes.jsx
@@ -1,6 +1,7 @@
 import Accordion from 'react-bootstrap/Accordion';
 import './planes.css';
-import Button from 'react-bootstrap/Button';
+import {Button }from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 function Planes() {
@@ -11,23 +12,26 @@ function Planes() {
         <Accordion.Item eventKey="0" className='estiloPlan'>
           <Accordion.Header className='tituloPlan'>Primeros pasos</Accordion.Header>
           <Accordion.Body>
-          Cuidado esencial y asequible que incluye exámenes anuales, vacunas clave y desparasitación para una protección básica y accesible de tu mascota.
+            Cuidado esencial y asequible que incluye exámenes anuales, vacunas clave y desparasitación para una protección básica y accesible de tu mascota.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1" className='estiloPlan'>
           <Accordion.Header className='tituloPlan'>Madurando</Accordion.Header>
           <Accordion.Body>
-          Lleva el cuidado al siguiente nivel con análisis tempranos, consultas ilimitadas y servicios ampliados para un cuidado completo y a medida de tu compañero peludo.
+            Lleva el cuidado al siguiente nivel con análisis tempranos, consultas ilimitadas y servicios ampliados para un cuidado completo y a medida de tu compañero peludo.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2" className='estiloPlan'>
           <Accordion.Header className='tituloPlan'>Adultos</Accordion.Header>
           <Accordion.Body>
-          Atención avanzada con exámenes exhaustivos, cirugías programadas, acceso a especialistas y terapias especializadas para garantizar el máximo bienestar y salud de tu mascota.
+            Atención avanzada con exámenes exhaustivos, cirugías programadas, acceso a especialistas y terapias especializadas para garantizar el máximo bienestar y salud de tu mascota.
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-      <Button className='botonPlanes' variant="primary" size="lg"> Ver Planes </Button>
+      <Button className='botonPlanes' variant="primary" size="lg">
+        <Link to="/detallePlanes">Ver Planes</Link>
+      </Button>
+
     </div>
   );
 }
--- huellitas/src/components/rutas/Rutas.jsx
+++ huellitas/src/components/rutas/Rutas.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 // import Error404 from "../../pages/Error404/Error404"; 
 // import ModalLogin from "../../components/Login/ModalLogin";
-import MisMascotas from "../../../pages/MisMascotas/MisMascotas";
+// import MisMascotas from "../pages/MisMascotas/MisMascotas";
+import DetallePlanes from "../../pages/detallePlanes/DetallePlanes";
 
 const Rutas = () => {
   return (
     <Routes>
-      {/* Ruta de redirección */}
+      
       <Route path="/index.html" element={null} />
 
-      {/* Ruta para la URL raíz ("/") */}
+      
       <Route path="/" element={<Navigate to="/index.html" />} />
+      
+      <Route path="/detallePlanes" element={<DetallePlanes />} />
 
       {/* 
       Ruta para ModalLogin
       <Route path="/login/modal" element={<ModalLogin />} />
       */}
 
-      <Route path="/mismascotas" element={<MisMascotas />} />
+      {/* <Route path="/mismascotas" element={<MisMascotas />} /> */}
 
-      {/* Otras rutas */}
+     
       {/* <Route path="/error404" element={<Error404 />} /> */}
 
-      {/* Redirección para rutas inexistentes */}
+      
       {/* <Route path="*" element={<Navigate to="/error404" />} /> */}
     </Routes>
 
